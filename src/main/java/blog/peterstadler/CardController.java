d2188226c7517be96c7c4e55c79b2a1f06b14708
Peter Stadler <14peterstadler@gmail.com>
2023-07-03 22:33:45 +0200

    $*

--- src/main/java/blog/peterstadler/CardController.java
+++ src/main/java/blog/peterstadler/CardController.java
@@ -1,13 +1,9 @@
 package blog.peterstadler.com.blog;
- 
 import java.util.*;
- 
- import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.http.*;
- 
 import org.springframework.web.bind.annotation.*;
  
 @RestController
@@ -15,10 +11,10 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "http://localhost:3000")
 public class CardController {
  
-    @Autowired
-    private CardService service;
+@Autowired
+private CardService service;
      
-    // RESTful API methods for Retrieval operations
+// RESTful API methods for Retrieval operations
 @GetMapping("/cards")
 public List<Card> list() {
     return service.listAll();
@@ -29,25 +25,22 @@ public Card get(@PathVariable Integer id) {
     return service.get(id);
 }
 
-
-    // RESTful API method for Create operation
+// RESTful API method for Create operation
 @PostMapping("/cards")
 public Card create(@RequestBody Card card) {
     return service.create(card);
 }
 
-    // RESTful API method for Update operation
- 
-    @PutMapping("/cards/{id}")
-    public Card replace(@RequestBody Card newCard, @PathVariable Integer id) {
-        return service.replace(newCard, id);
-    }
+// RESTful API method for Update operation
+@PutMapping("/cards/{id}")
+public Card replace(@RequestBody Card newCard, @PathVariable Integer id) {
+    return service.replace(newCard, id);
+}
 
-    // RESTful API method for Delete operation
+// RESTful API method for Delete operation
 @DeleteMapping("/cards/{id}")
 public void delete(@PathVariable Integer id) {
     service.delete(id);
 }
 
- 
 }
